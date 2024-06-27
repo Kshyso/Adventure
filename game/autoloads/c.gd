99d53b8c0055fa4d78676c9478ce8c7a430a46e7
Kshyso <Kshysio@protonmail.com>
2024-06-27 19:03:00 +0200

    Testing

--- game/autoloads/c.gd
+++ game/autoloads/c.gd
@@ -2,11 +2,14 @@
 extends "res://addons/popochiu/engine/interfaces/i_character.gd"
 
 # classes ----
+const PCGuy := preload("res://game/characters/guy/character_guy.gd")
 # ---- classes
 
 # nodes ----
+var Guy: PCGuy : get = get_Guy
 # ---- nodes
 
 # functions ----
+func get_Guy() -> PCGuy: return super.get_runtime_character("Guy")
 # ---- functions
 
