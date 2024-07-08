ec654e33d97f4caa2222b927cfdb7f5c135e649e
Kshyso <Kshysio@protonmail.com>
2024-07-08 16:30:27 +0200

    Changed everything

---
+++ Scripts/mc.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed : float = 200.0
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		#print(event.global_position)
+		$NavigationAgent2D.target_position = event.global_position
+		#print($NavigationAgent2D.get_next_path_position())
+
+func _physics_process(_delta: float) -> void:
+	# Calculate the direction to the target position
+	var direction: Vector2 = ($NavigationAgent2D.get_next_path_position() - global_position).normalized()
+	# Calculate the distance to the target position
+	var distance: float = global_position.distance_to($NavigationAgent2D.get_next_path_position())
+	
+	# Move the character towards the target position at a constant speed
+	if distance > 2.0:  # Check if we are close enough to the target position
+		velocity = direction * speed
+		move_and_slide()
+
+	# Optional: print the current position
+	#print("Current Position: ", global_position)
